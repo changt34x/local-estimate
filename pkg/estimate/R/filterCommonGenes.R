da4cdd407e9f18af1fde7cff533204b6045462c8
changt34x <changt34x@gmail.com>
2017-06-06 20:08:00 -0500

    dataframe io for filterCommonGenes

--- pkg/estimate/R/filterCommonGenes.R
+++ pkg/estimate/R/filterCommonGenes.R
@@ -5,22 +5,30 @@
 
 ##-----------------------------------------------------------------------------
 filterCommonGenes <- function(input.f,
-                              output.f,
-                              id=c("GeneSymbol", "EntrezID")) {
+                              output.f=NULL,
+                              id=c("GeneSymbol", "EntrezID"),
+                              writeToDisk=FALSE) {
 
     ## Check arguments
     stopifnot((is.character(input.f) && length(input.f) == 1 && nzchar(input.f)) ||
-              (inherits(input.f, "connection") && isOpen(input.f, "r")))
-    stopifnot((is.character(output.f) && length(output.f) == 1 && nzchar(output.f)))
-    id <- match.arg(id)   
+              (inherits(input.f, "connection") && isOpen(input.f, "r")) ||
+              inherits(input.f, "data.frame"))
+    if (writeToDisk) {
+      stopifnot((is.character(output.f) && length(output.f) == 1 && nzchar(output.f)))
+    }
+    id <- match.arg(id)
      
     ## Read input data
-    input.df <- read.table(input.f,
-                           header=TRUE,
-                           row.names=1,
-                           sep="\t", 
-                           quote="",
-                           stringsAsFactors=FALSE)
+    if (inherits(input.fm "data.frame") == FALSE) {
+      input.df <- read.table(input.f,
+                             header=TRUE,
+                             row.names=1,
+                             sep="\t", 
+                             quote="",
+                             stringsAsFactors=FALSE)
+    } else {
+      input.df <- input.f
+    }
      
     merged.df <- merge(common_genes, input.df, by.x=id, by.y="row.names")
     rownames(merged.df) <- merged.df$GeneSymbol
@@ -28,6 +36,12 @@ filterCommonGenes <- function(input.f,
     print(sprintf("Merged dataset includes %d genes (%d mismatched).",
                   nrow(merged.df),
                   nrow(common_genes) - nrow(merged.df)))
-    outputGCT(merged.df, output.f)
+
+    ## Output data
+    if (writeToDisk) {
+      outputGCT(merged.df, output.f)
+    } else {
+      return(merged.df)
+    }
 }
 
